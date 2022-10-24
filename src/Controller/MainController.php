8286a3b932d48ecca87c3d7a1ea3b8a5c5919a73
Akila061 <arthaud.frederic@gmail.com>
2022-10-24 17:00:47 +0200

    commit de fin de journée

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Entity\Wish;
+use App\Form\SerieType;
+use App\Form\WishType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,5 +31,33 @@ class MainController extends AbstractController
     public function legal(): Response
     {
         return $this->render('main/legal.html.twig');
+    }
+
+    #[Route('/new-wish', name: 'new_wish')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $wish = new Wish();
+        $wish->setDateCreated(new \DateTime()); // Ou utiliser les LifeCycleCallbacks de Doctrine
+        $wishForm = $this->createForm(WishType::class, $wish);
+
+        // Récupération des données pour les insérer dans l'objet $serie
+        $wishForm->handleRequest($request);
+        dump($wish);
+
+        // Vérifier si l'utilisateur est en train d'envoyer le formulaire
+        if ($wishForm->isSubmitted() && $wishForm->isValid()) {
+            // Enregistrer la nouvelle série en BDD
+            $em->persist($wish);
+            $em->flush();
+
+            $this->addFlash('success', 'l\'idée a bien été créée !');
+
+            // Rediriger l'internaute vers la liste des séries
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('main/new.html.twig', [
+            'wishForm' => $wishForm->createView()
+        ]);
     }
 }
